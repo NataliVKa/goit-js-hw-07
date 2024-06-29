73e273c6a7a7f649acd90ff59a04c232855f3470
NataliVKa <153003372+NataliVKa@users.noreply.github.com>
2024-06-29 18:57:16 +0300

    startHomeWork7

---
+++ js/task-4.js
@@ -0,0 +1,19 @@
+const form = document.querySelector("form");
+const emailInput = document.querySelector('input[name="email"]');
+const passwordInput = document.querySelector('input[name="password"]');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  if (emailInput.value.trim() === "" || passwordInput.value.trim() === "") {
+    alert("All form fields must be filled in");
+  } else {
+    const user = {
+      email: emailInput.value.trim(),
+      password: passwordInput.value.trim(),
+    };
+
+    console.log(user);
+    form.reset();
+  }
+});
---
+++ js/task-5.js
@@ -0,0 +1,15 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const body = document.querySelector("body");
+const changeBtn = document.querySelector(".change-color");
+const span = document.querySelector(".color");
+
+changeBtn.addEventListener("click", (event) => {
+  const hexColor = getRandomHexColor();
+  body.style.backgroundColor = hexColor;
+  span.textContent = hexColor;
+});
